a70b54d3ea9f4a147c6e70f05cedfa9e53e6b097
lucabeca <lucas.17aac16@gmail.com>
2023-06-17 18:12:28 -0300

    saindo

--- src/gestaocomercial/model/list/Estoque.java
+++ src/gestaocomercial/model/list/Estoque.java
@@ -1,5 +1,18 @@
 package gestaocomercial.model.list;
 
-public class Estoque {
+import java.util.ArrayList;
+import java.util.List;
 
+import gestaocomercial.dto.ProdutoEmEstoque;
+
+public class Estoque {
+	private List<ProdutoEmEstoque> listaDeProdutosEmEstoque = new ArrayList<ProdutoEmEstoque>();
+
+	public List<ProdutoEmEstoque> getListaDeProdutosEmEstoque() {
+		return listaDeProdutosEmEstoque;
+	}
+
+	public void setListaDeProdutosEmEstoque(List<ProdutoEmEstoque> listaDeProdutosEmEstoque) {
+		this.listaDeProdutosEmEstoque = listaDeProdutosEmEstoque;
+	}
 }
