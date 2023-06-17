57660842a28a1c9f35c9dce8236ff656eddc48b9
lucabeca <lucas.17aac16@gmail.com>
2023-06-17 16:19:01 -0300

    Alterado compradores e menu de compradores

--- src/gestaocomercial/application/MenuComprador.java
+++ src/gestaocomercial/application/MenuComprador.java
@@ -3,6 +3,7 @@ package gestaocomercial.application;
 import javax.swing.JOptionPane;
 
 import gestaocomercial.dto.BancoDeDados;
+import gestaocomercial.model.Comprador;
 
 public class MenuComprador {
 	public static void exibirMenuCompradores(BancoDeDados bancoDeDados) {
@@ -13,13 +14,21 @@ public class MenuComprador {
 					+ "----------- MENU COMPRADOR -----------\n"
 					+ "Insira a opção que deseja realizar:\n"
 					+ "1 - Cadastrar novo comprador\n"
-					+ "2 - Consultar um comprador"
-					+ "3 - Alterar cadastro"
-					+ "4 - Voltar"));
+					+ "2 - Consultar um comprador\n"
+					+ "3 - Alterar cadastro\n"
+					+ "4 - Voltar\n"));
 
 			switch(opcao) {
 			case 1:
-				Comprador();
+				Comprador comprador = new Comprador();
+				comprador.setNome(JOptionPane.showInputDialog("Insira o Nome do comprador: "));
+				comprador.setEmail(JOptionPane.showInputDialog("Insira o Email do comprador:"));
+				comprador.setTelefone(JOptionPane.showInputDialog("Insira o Telefone do comprador:"));
+				comprador.setCpfCnpj(JOptionPane.showInputDialog("Insira o CPF/CNPJ do comprador:"));
+				comprador.setObs(JOptionPane.showInputDialog("Insira uma Observação sobre o comprador:"));
+				
+				
+				bancoDeDados.getListaDeCompradores().getListaDeCompradores().add(comprador);
 				break;
 			case 2:
 				exibirComprador();
@@ -31,12 +40,11 @@ public class MenuComprador {
 				MenuPrincipal.exibirMenuPrincipal();
 				JOptionPane.showMessageDialog(null, "Voltando para o menu principal...");
 				break;
-				break;
 			default:
 				JOptionPane.showMessageDialog(null, "Opção inválida inserida, tente novamente\n");
 				exibirMenuPrincipal();
 			}
 
-		} while(opcao != 6);
+		} while(opcao != 4);
 	}
 }
--- src/gestaocomercial/model/list/CompradoresAtuais.java
+++ src/gestaocomercial/model/list/CompradoresAtuais.java
@@ -6,4 +6,12 @@ import gestaocomercial.model.Comprador;
 
 public class CompradoresAtuais {
 	List<Comprador> listaDeCompradores;
+
+	public List<Comprador> getListaDeCompradores() {
+		return listaDeCompradores;
+	}
+
+	public void setListaDeCompradores(List<Comprador> listaDeCompradores) {
+		this.listaDeCompradores = listaDeCompradores;
+	}
 }
